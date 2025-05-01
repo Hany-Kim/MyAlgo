97033db4bea64eb4878add13cfa10b4030e9a660
Hans <hanykim1210@gmail.com>
2025-05-01 16:13:01 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 144 ms, Memory: 15328 KB -BaekjoonHub

---
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -0,0 +1,54 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int[] arr;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        dp = new int[2][N]; // row 0 : 오름차순, row 1 : 내림차순
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            dp[0][i] = 1;
+            dp[1][i] = 1;
+        }
+
+        for(int i=1; i<N; i++){
+            for(int j=i-1; j>=0; j--){
+                if(arr[i] > arr[j]){
+                    // 오름차순 인지, 오름차순 시작 지점인지
+                    dp[0][i] = Math.max(dp[0][j] + 1, dp[0][i]);
+                }
+                else if(arr[i] < arr[j]){
+                    // 그대로 인지, 내림차순 이어지는 지점 인지, 내림차순으로 변경된지점인지
+                    dp[1][i] = Math.max(dp[1][i], Math.max(dp[1][j] + 1, dp[0][j] + 1));
+                }
+                else if(arr[i] == arr[j]){
+                    dp[0][i] = Math.max(dp[0][i], dp[0][j]);
+                    dp[1][i] = Math.max(dp[1][i], dp[1][j]);
+                }
+            }
+        }
+
+        int ans = 0;
+        for(int i=0; i<2; i++){
+            for(int j=0; j<N; j++){
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+
+        bw.write(String.valueOf(ans));
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
