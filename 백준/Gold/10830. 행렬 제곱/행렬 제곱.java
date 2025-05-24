6bb6cf119019a542f8166818e3c566aa1e38232c
Hans <hanykim1210@gmail.com>
2025-05-24 15:48:44 +0900

    [Gold IV] Title: 행렬 제곱, Time: 156 ms, Memory: 15860 KB -BaekjoonHub

---
+++ 백준/Gold/10830. 행렬 제곱/행렬 제곱.java
@@ -0,0 +1,83 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n;
+    static Long b;
+    static int[][] matrixA;
+    static int mod = 1000;
+
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        b = Long.parseLong(st.nextToken());
+        matrixA = new int[n][n];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<n; j++){
+                matrixA[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        if (b == 1){
+            for(int y=0; y<n; y++){
+                for(int x=0; x<n; x++){
+                    int num = matrixA[y][x] % mod;
+                    bw.write(num + " ");
+                }
+                bw.write("\n");
+            }
+        }
+        else {
+            int[][] ans = func(matrixA, b);
+
+            for(int y=0; y<n; y++){
+                for(int x=0; x<n; x++){
+                    bw.write(ans[y][x] + " ");
+                }
+                bw.write("\n");
+            }
+        }
+
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static int[][] func(int[][] now, Long b) {
+        if(b == 1){
+            return matrixA;
+        }
+
+        int[][] newMatrix = func(now, b / 2);
+        if(b%2 == 0){
+            return multiplicationMatrixAAndB(newMatrix, newMatrix);
+        }
+        else {
+            return multiplicationMatrixAAndB(newMatrix, multiplicationMatrixAAndB(newMatrix, matrixA));
+        }
+    }
+
+    private static int[][] multiplicationMatrixAAndB(int[][] a, int[][] b) {
+        int[][] temp = new int[n][n];
+
+        for(int y=0; y<n; y++){
+            for(int i=0; i<n; i++){
+                int num = 0;
+                for(int x=0; x<n; x++){
+                    num += ((a[y][x] * b[x][i]) % mod);
+                }
+                temp[y][i] = num % mod;
+            }
+        }
+
+        return temp;
+    }
+}
