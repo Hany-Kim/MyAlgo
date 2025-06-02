3f091fa4f7fcfb6438d65b164c435c93078bb930
Hans <hanykim1210@gmail.com>
2025-06-02 21:53:12 +0900

    [Silver III] Title: 줄줄이 박수, Time: 764 ms, Memory: 251540 KB -BaekjoonHub

--- 백준/Silver/29718. 줄줄이 박수/줄줄이 박수.java
+++ 백준/Silver/29718. 줄줄이 박수/줄줄이 박수.java
@@ -3,8 +3,9 @@ import java.util.*;
 
 public class Main {
     static int n, m;
-    static int[][] map;
     static int seg;
+    static int[] arr;
+    static int[] segArr;
 
     public static void main(String[] args) throws IOException {
 //        BufferedReader br = new BufferedReader(new FileReader("src/input.txt"));
@@ -15,33 +16,24 @@ public class Main {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        map = new int[n][m];
+        arr = new int[m];
         for(int y=0; y<n; y++){
             st = new StringTokenizer(br.readLine());
             for(int x=0; x<m; x++){
-                map[y][x] = Integer.parseInt(st.nextToken());
+                arr[x] += Integer.parseInt(st.nextToken());
             }
         }
         seg = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[m];
-        for(int x=0; x<m; x++){
-            for(int y=0; y<n; y++){
-                arr[x] += map[y][x];
-            }
-        }
-
         int sIdx = 0;
         int eIdx = sIdx + seg;
 
-        int[] segArr = new int[m - seg + 1];
-
+        segArr = new int[m - seg + 1];
         for(int i=sIdx; i<eIdx; i++){
             segArr[0] += arr[i];
         }
 
         int maxNum = segArr[0];
-
         for(int j=1; j<(m-seg+1); j++){
             segArr[j] = segArr[j - 1] - arr[sIdx] + arr[eIdx];
             maxNum = Math.max(maxNum, segArr[j]);
